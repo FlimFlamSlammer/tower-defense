e30dc6378aba4cec4aa99d1a45b69bbfb0f1a22d
Lionel Wijaya <lionelchris1509@gmail.com>
2025-01-27 10:17:39 +0700

    feat: added fixed width outline to tower range circles

---
+++ scripts/unscaled_outline.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+@export var thickness: float = 1.0
+@export var color := Vector4(1.0, 1.0, 1.0, 1.0)
+
+func _ready() -> void:
+	material = preload("res://materials/outline.tres").duplicate()
+
+func _process(_delta: float) -> void:
+	if not is_visible_in_tree():
+		return
+		
+	var scaled_thickness: float = thickness / global_scale.x
+	var shader_material := material as ShaderMaterial
+	shader_material.set_shader_parameter("line_thickness", scaled_thickness)
+	shader_material.set_shader_parameter("line_color", color)
