3e3dffe6a22c8ebd69daf19c742facb70c27f3b0
Lionel Wijaya <lionel_cw@proton.me>
2025-03-26 20:03:20 +0700

    feat: change lives and money variables to integers

--- scripts/game-provider.gd
+++ scripts/game-provider.gd
@@ -1,14 +1,15 @@
 class_name GameProvider
 extends Node
 
-var lives: float = 100.0:
+var lives: int = 100:
 	set(val):
 		lives = val
-		gui.lives_display.value = str(roundi(lives))
-var money: float = 200.0:
+		gui.lives_display.value = str(lives)
+
+var money: int = 200:
 	set(val):
 		money = val
-		gui.money_display.value = str(roundi(money))
+		gui.money_display.value = str(money)
 
 @onready var spawner: Spawner = $MainTileMap/Spawner
 @onready var gui: GUIRoot = $GUILayer/GUI
@@ -16,6 +17,6 @@ var money: float = 200.0:
 func _ready() -> void:
 	spawner.enemySpawned.connect(func(enemy: Enemy):
 		enemy.enemyLeaked.connect(func(health: float):
-			lives -= health
+			lives -= max(roundi(health), 1)
 		)
 	)
