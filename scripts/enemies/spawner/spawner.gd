97386abc536969358d2ecce3330d37cea43e2ff2
Lionel Wijaya <lionel_cw@proton.me>
2025-04-22 19:50:37 +0700

    fix: wave detected as over when there are no enemies on screen, even when the wave spawner is still spawning

--- scripts/enemies/spawner/spawner.gd
+++ scripts/enemies/spawner/spawner.gd
@@ -2,11 +2,17 @@ class_name Spawner
 extends Node
 
 signal enemy_spawned(enemy: Enemy)
+signal wave_started()
+signal wave_ended()
 
 @export var wave_data: WaveData
-var wave: int = 0
+
+var wave: int = 0 ## The current wave (zero-indexed)
+var spawning: bool = false ## Whether or not this [Spawner] is currently spawning
 
 var _wave_spawner: PackedScene = preload("uid://bsxu1mlbbwsws")
+
+var _remaining_wave_spawners: int = 0
 
 @onready var tile_controller := get_node(Globals.TILE_CONTROLLER_PATH) as TileController
 
@@ -14,5 +20,18 @@ var _wave_spawner: PackedScene = preload("uid://bsxu1mlbbwsws")
 func start_wave() -> void:
 	wave += 1;
 	var wave_spawner: WaveSpawner = _wave_spawner.instantiate()
+
 	wave_spawner.wave_data = wave_data.data[wave - 1] # zero-indexing
+	wave_spawner.enemy_spawned.connect(enemy_spawned.emit)
+
+	_remaining_wave_spawners += 1
+	wave_started.emit()
+
 	add_child(wave_spawner)
+
+
+func _on_wave_spawner_finished():
+	_remaining_wave_spawners -= 1
+	wave_ended.emit()
+	if _remaining_wave_spawners == 0:
+		spawning = false
--- scripts/enemies/spawner/wave_spawner.gd
+++ scripts/enemies/spawner/wave_spawner.gd
@@ -1,24 +1,38 @@
 class_name WaveSpawner
 extends Node
 
+signal finished()
+signal enemy_spawned(enemy: Enemy)
+
 var wave_data: Array
 
 var _time_since_wave_start: float = 0.0
 var _group_index: int = 0
+var _groups_finished: int = 0
 
 var _group_spawner: PackedScene = preload("uid://b616w7ivj0t6t")
 
-@onready var start_tile: Vector2i = (get_node(Globals.TILE_CONTROLLER_PATH) as TileController).start_tile
+@onready var _start_tile: Vector2i = (get_node(Globals.TILE_CONTROLLER_PATH) as TileController).start_tile
 
 func _process(delta: float) -> void:
 	_time_since_wave_start += delta
 
+	
 	while _group_index < wave_data.size() and _time_since_wave_start >= wave_data[_group_index].time:
-		var group_spawner: GroupSpawner = _group_spawner.instantiate()
-		group_spawner.group = wave_data[_group_index]
+		var group_spawner: GroupSpawner
+		group_spawner = _group_spawner.instantiate()
+		group_spawner.group_data = wave_data[_group_index]
+
+		group_spawner.finished.connect(_on_group_spawner_finish)
+		group_spawner.enemy_spawned.connect(enemy_spawned.emit)
+
 		add_sibling(group_spawner)
 
 		_group_index += 1;
 
-	if _group_index == wave_data.size():
+
+func _on_group_spawner_finish():
+	_groups_finished += 1
+	if _groups_finished == wave_data.size():
 		queue_free()
+		finished.emit()
