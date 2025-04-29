584164003a776f5befd56ae65e7fb1febb3bd53c
Lionel Wijaya <lionel_cw@proton.me>
2025-04-29 21:18:50 +0700

    fix: lose menu opening when pause button is pressed

--- scripts/ui/screen_menus/lose_menu.gd
+++ scripts/ui/screen_menus/lose_menu.gd
@@ -5,17 +5,6 @@ signal restart_requested()
 signal exit_requested()
 signal retry_requested()
 
-@onready var _pause_menu_button: Button = get_tree().get_nodes_in_group("pause_menu_buttons")[-1]
-
-func _ready() -> void:
-	_pause_menu_button.pressed.connect(open)
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
-		close()
-
-
 func _on_retry_button_pressed() -> void:
 	retry_requested.emit()
 
