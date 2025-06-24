453045c10810e05ad0cf963ad7460ca59df0fa0f
Lionel Wijaya <lionel_cw@proton.me>
2025-06-24 11:30:13 +0700

    fix: support towers not updating applied buffs on upgrade

--- scripts/towers/support_tower.gd
+++ scripts/towers/support_tower.gd
@@ -5,6 +5,11 @@ func _can_affect_tower(tower: Tower, status_effect: TowerStatusEffect) -> bool:
 	return true
 
 
+func update_status_effects() -> void:
+	super ()
+	give_status_effects()
+
+
 ## Applies all status effects in child [code]StatusEffects[/code] to nearby towers.
 func give_status_effects() -> void:
 	if not _placed: return
@@ -31,7 +36,7 @@ func give_status_effects() -> void:
 				for i: int in range(effect_paths.size()):
 					if effect_paths[i] is NodePath:
 						effect_paths[i] = status_effect.get_node(effect_paths[i])
-				
+
 				if status_effect.id in tower.status_effects \
 						and tower.status_effects[status_effect.id].priority == status_effect.priority:
 					tower.remove_status_effect(status_effect.id)
