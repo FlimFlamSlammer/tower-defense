2ac69b572940130d993238e2519fd42919a9094e
Lionel Wijaya <lionel_cw@proton.me>
2025-06-24 11:31:39 +0700

    feat: add enemy exceptions to projectiles

--- scripts/projectiles/projectile.gd
+++ scripts/projectiles/projectile.gd
@@ -8,6 +8,7 @@ var stats: Dictionary[StringName, Variant]
 var movement_dir: float
 
 var _pierce_used: int = 0
+var _exceptions: Dictionary[Enemy, bool]
 
 func _ready() -> void:
 	rotation = movement_dir
@@ -21,30 +22,45 @@ func _ready() -> void:
 		stats.projectile_status_effects = status_effects
 
 
+func add_exception(enemy: Enemy) -> void:
+	_exceptions[enemy] = true
+	enemy.tree_exited.connect(remove_exception.bind(enemy))
+
+
+func remove_exception(enemy: Enemy) -> void:
+	_exceptions.erase(enemy)
+	enemy.tree_exited.disconnect(remove_exception.bind(enemy))
+
+
+func has_exception(enemy: Enemy) -> bool:
+	return enemy in _exceptions
+
+
 func _on_collision(area: Area2D) -> void:
 	if _pierce_used >= stats.pierce:
 		return
 
 	var enemy := area as Enemy
-	if enemy:
-		var pierce_damage_multiplier: float = 1.0 - ((1.0 - MIN_PIERCE_DAMAGE_MULTIPLIER) * _pierce_used / stats.pierce)
-		var armor_piercing_damage_multiplier: float = 1.0
+	if not enemy or has_exception(enemy): return
 
-		if "armor_piercing" in stats:
-			armor_piercing_damage_multiplier = 1.0 - stats.armor_piercing
-			enemy.hit(stats.damage * pierce_damage_multiplier * stats.armor_piercing, Globals.DamageTypes.NORMAL)
+	var pierce_damage_multiplier: float = 1.0 - ((1.0 - MIN_PIERCE_DAMAGE_MULTIPLIER) * _pierce_used / stats.pierce)
+	var armor_piercing_damage_multiplier: float = 1.0
 
-		enemy.hit(stats.damage * pierce_damage_multiplier * armor_piercing_damage_multiplier, stats.damage_type)
-		print(stats.damage * pierce_damage_multiplier * armor_piercing_damage_multiplier)
-		_pierce_used += 1
+	if "armor_piercing" in stats:
+		armor_piercing_damage_multiplier = 1.0 - stats.armor_piercing
+		enemy.hit(stats.damage * pierce_damage_multiplier * stats.armor_piercing, Globals.DamageTypes.NORMAL)
 
-		if "projectile_status_effects" in stats:
-			var status_effects: Array[Variant] = stats.projectile_status_effects
-			for effect: EnemyStatusEffect in status_effects:
-				if _pierce_used == stats.pierce:
-					enemy.apply_status_effect(effect)
-				else:
-					enemy.apply_status_effect(effect.duplicate())
+	enemy.hit(stats.damage * pierce_damage_multiplier * armor_piercing_damage_multiplier, stats.damage_type)
+	print(stats.damage * pierce_damage_multiplier * armor_piercing_damage_multiplier)
+	_pierce_used += 1
+
+	if "projectile_status_effects" in stats:
+		var status_effects: Array[Variant] = stats.projectile_status_effects
+		for effect: EnemyStatusEffect in status_effects:
+			if _pierce_used == stats.pierce:
+				enemy.apply_status_effect(effect)
+			else:
+				enemy.apply_status_effect(effect.duplicate())
 
 
 	if _pierce_used == stats.pierce:
